840649ce02b69ed1afdcd11fcbeeceb1c83836ed
Ajit Singh <68308589+AjitSingh-Brar@users.noreply.github.com>
2021-03-21 17:09:34 -0400

    AADD: cart total HTML/ style

--- src/components/CardTotal.js
+++ src/components/CardTotal.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./CardTotal.css";
 
 function CardTotal() {
-  return <div className="CardTotal"></div>
+  return <div className="CardTotal">
+    <h3>Subtotal(5 items):
+      <span className="CardTotal-price">
+        $2,172.38
+      </span>
+    </h3>
+    <button>
+      Proceed to Checkout
+    </button>
+  </div>
 }
 
 export default CardTotal;
