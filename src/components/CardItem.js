8b0e5dc8bb87c6a8ea8d6ab1a58fa25d8e3a2452
Ajit Singh <68308589+AjitSingh-Brar@users.noreply.github.com>
2021-03-21 15:28:02 -0400

    ADD: Styling for CardItem

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -16,8 +16,10 @@ function CardItem() {
                         <select>
                             <option value="1">Qty: 1</option>
                             <option value="2">Qty: 2</option>
+                            <option value="3">Qty: 3</option>
                         </select>
                     </div>
+                    <div className="item-actions-divider">|</div>
                     <div className="item-delete">
                         Delete
                     </div>
