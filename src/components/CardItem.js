275aea3f108aa90ffacfb2fc53297e7c1675d103
Ajit Singh <68308589+AjitSingh-Brar@users.noreply.github.com>
2021-03-21 21:31:48 -0400

    CHNG: Format total price

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,37 +1,46 @@
-import React, { useState } from 'react'
-import "./CardItem.css"
-function CardItem({item}) {
+import React, { useState } from "react";
+import { useStateValue } from "../StateProvider";
+import "./CardItem.css";
+function CardItem({ item }) {
+  const [cartItem, setCartItem] = useState(item);
+  //   const [{ basket }, dispatch] = useStateValue();
 
-    const [cartItem, setCartItem] = useState(item);
-    return (
-        <div className="CardItem">
-            <div className="CardItem-image">
-                <img src={process.env.PUBLIC_URL + `/items/${cartItem.image}`} alt=""/>
-            </div>
-            <div className="CardItem-info">
-                <div className="info-title">
-                    <h2>{cartItem.title}</h2>
-                </div>
-                <div className="info-stock">{cartItem.stock}</div>
-                <div className="item-actions">
-                    <div className="item-quantity">
-                        <select value = {cartItem.quantity}>
-                            <option value="1">Qty: 1</option>
-                            <option value="2">Qty: 2</option>
-                            <option value="3">Qty: 3</option>
-                        </select>
-                    </div>
-                    <div className="item-actions-divider">|</div>
-                    <div className="item-delete">
-                        Delete
-                    </div>
-                </div>
-            </div>
-            <div className="CardItem-price">
-               ${cartItem.price}
-            </div>
+  //   console.log(basket);
+  //   const addToBasket = (e) => {
+  //     let quantity = parseInt(e.target.value);
+  //     //dispatch the quantity into the data layer
+  //     dispatch({
+  //       type: "ADD_TO_BASKET",
+  //       item: quantity,
+  //     });
+  //     console.log(quantity);
+  //   };
+
+  return (
+    <div className="CardItem">
+      <div className="CardItem-image">
+        <img src={process.env.PUBLIC_URL + `/items/${cartItem.image}`} alt="" />
+      </div>
+      <div className="CardItem-info">
+        <div className="info-title">
+          <h2>{cartItem.title}</h2>
         </div>
-    )
+        <div className="info-stock">{cartItem.stock}</div>
+        <div className="item-actions">
+          <div className="item-quantity">
+            <select value={cartItem.quantity}>
+              <option value="1">Qty: 1</option>
+              <option value="2">Qty: 2</option>
+              <option value="3">Qty: 3</option>
+            </select>
+          </div>
+          <div className="item-actions-divider">|</div>
+          <div className="item-delete">Delete</div>
+        </div>
+      </div>
+      <div className="CardItem-price">${cartItem.price}</div>
+    </div>
+  );
 }
 
-export default CardItem
+export default CardItem;
--- src/components/CardTotal.js
+++ src/components/CardTotal.js
@@ -1,26 +1,27 @@
 import React from "react";
 import "./CardTotal.css";
+import NumberFormat from "react-number-format";
 
-function CardTotal({items}) {
-
-  const getTotalPrice = ()=>{
+function CardTotal({ items }) {
+  const getTotalPrice = () => {
     let total = 0;
-    items.map((item)=>{
-      total = total  +  (item.price * item.quantity);
-    })
-    return total.toFixed(2);
-  }
+    items.forEach((item) => {
+      total = total + item.price * item.quantity;
+    });
+    return total;
+  };
 
-  return <div className="CardTotal">
-    <h3>Subtotal({items.length} items):
-      <span className="CardTotal-price">
-        ${getTotalPrice()}
-      </span>
-    </h3>
-    <button>
-      Proceed to Checkout
-    </button>
-  </div>
+  return (
+    <div className="CardTotal">
+      <h3>
+        Subtotal({items.length} items):
+        <span className="CardTotal-price">
+          <NumberFormat value={getTotalPrice()} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale = {2}/>
+        </span>
+      </h3>
+      <button>Proceed to Checkout</button>
+    </div>
+  );
 }
 
 export default CardTotal;
